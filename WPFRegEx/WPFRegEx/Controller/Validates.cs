2e33cd0497d6b512253f2a5b049a6951a75e3f5a
Treinamento HBSIS <treinamento@hbsis.com.br>
2019-08-08 15:05:46 -0300

    Usando regex email e telefone

---
+++ WPFRegEx/WPFRegEx/Controller/Validates.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WPFRegEx.Controller
+{
+    public class Validates
+    {
+        public static bool ValidEmail(string email)
+        {
+            var regExStringEmail = @"^[\w!#$%&'*+\-/=?\^_`{|}~]+(\.[\w!#$%&'*+\-/=?\^_`{|}~]+)*"
+                                 + "@"
+                                 + @"((([\w]+([-\w]*[\w]+)*\.)+[a-zA-Z]{2,4})|"
+                                 + @"((([01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5]).){3}[01]?[0-9]{1,2}|2[0-4][0-9]|25[0-5]))\z";
+
+            Regex regexMail = new Regex(regExStringEmail);
+
+
+            return regexMail.IsMatch(email);
+        }
+
+        public static bool ValidPhone(string telefone)
+        {
+            var regExTelefone = @"^(?:(55\d{2})|\d{2})[6-9]\d{8}$";
+
+            Regex regexPhone = new Regex(regExTelefone);
+
+
+            return Regex.IsMatch(telefone, regExTelefone);
+
+        }
+    }
+}
---
+++ WPFRegEx/WPFRegEx/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WPFRegEx.Controller;
+
+namespace WPFRegEx
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtSalvar_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            if (Validates.ValidEmail(tbEmail.Text))
+            {
+                MessageBox.Show("certo");
+            }
+            if (Validates.ValidPhone(tbTelefone.Text))
+            {
+                MessageBox.Show("certo fone");
+            }
+            StringBuilder comparar = new StringBuilder();
+            comparar.Append(tbNome.Text);
+            comparar.Append(tbTelefone.Text);
+            comparar.Append(tbEmail.Text);
+
+
+            MessageBox.Show(comparar.ToString());
+        }
+
+     
+    }
+}
